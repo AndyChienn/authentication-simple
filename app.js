59209ed13bf5e9d7f1c74f8f16e3e01855bcc204
AndyChienn <andy2002chien@hotmail.com.tw>
2022-05-26 18:46:43 +0800

    feat: 新增保持登入功能

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const app = express()
+const session = require('express-session')
+
 const port = 3000
 const Account = require('./models/Account')
+
+// const session = require('express-session')
 
 const mongoose = require('mongoose')
 const db = mongoose.connection
@@ -13,6 +17,12 @@ app.set('view engine', 'handlebars')
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('public'))
+app.use(session({
+  secret: 'test session',
+  resave: false,
+  saveUninitialized: true,
+  // cookie: { secure: true }
+}))
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
@@ -25,6 +35,12 @@ db.once('open', () => {
 })
 
 app.get('/', (req, res) => {
+  if (req.session.login) {
+    console.log('loging yes')
+    const sessionName = req.session.name
+    console.log('session name', req.session.name)
+    return res.render('loginin', { sessionName: sessionName })
+  }
   res.render('index')
 })
 
@@ -36,6 +52,17 @@ app.post('/', (req, res) => {
       if (!user) {
         res.render('fail')
       } else if (user) {
+        // res.cookie('username', '001')
+        // res.status(200).send('登入成功')
+        console.log('session', req.session)
+        req.session.login = true
+        console.log('session new', req.session)
+        // user.sessionID = req.sessionID
+        req.session.name = user.email
+        // console.log('sessionID', req.sessionID)
+        // console.log('usersessionID', user.sessionID)
+        console.log(user)
+
         res.render('loginin', { userName: user.firstName })
       }
     })
